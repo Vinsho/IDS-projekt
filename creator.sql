4f0784436793a89c26b557bb0710daa8f8fa531a
vinso <jakub.vins2@gmail.com>
2019-03-27 16:35:29 +0100

    Changes to Klan_leader

--- creator.sql
+++ creator.sql
@@ -11,6 +11,14 @@ DROP TABLE Tim_vyhral_zapas CASCADE CONSTRAINTS;
 DROP TABLE Tim_sa_zucastni_turnaja CASCADE CONSTRAINTS;
 DROP TABLE Tim_sa_zucastni_zapasu CASCADE CONSTRAINTS;
 
+CREATE TABLE Osoba(
+  RodneCislo Number NOT NULL PRIMARY KEY,
+  Meno VARCHAR2(50) NOT NULL,
+  Priezvisko VARCHAR2(50) NOT NULL,
+  Narodnost VARCHAR(50) NOT NULL,
+  DatumNarodenia Date NOT NULL
+);
+
 CREATE TABLE Hra(
   ID_Hry NUMBER NOT NULL PRIMARY KEY,
   Nazov VARCHAR2(50),
@@ -34,14 +42,14 @@ CREATE TABLE Klan(
   Narodnost VARCHAR2(50),
   Hymna VARCHAR2(50),
   Logo VARCHAR2(50),
-  VodcaKlanu VARCHAR2(50),
   Hra Number NOT NULL,
+  Osoba NUMBER NOT NULL,
+  CONSTRAINT klan_osoba_FK FOREIGN KEY (Osoba) REFERENCES Osoba,
   CONSTRAINT klan_hra_FK FOREIGN KEY (Hra) REFERENCES Hra
 );
 
 CREATE TABLE Tim(
-  ID_Timu NUMBER NOT NULL PRIMARY KEY,
-  NAZOV VARCHAR2(50) NOT NULL
+  Nazov_Timu VARCHAR2(50) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE Turnaj(
@@ -59,67 +67,67 @@ CREATE TABLE Zapas(
 
 );
 
-CREATE TABLE Osoba(
-RodneCislo Number NOT NULL PRIMARY KEY,
-Meno VARCHAR2(50) NOT NULL,
-Priezvisko VARCHAR2(50) NOT NULL,
-Narodnost VARCHAR(50) NOT NULL,
-DatumNarodenia Date NOT NULL
-);
-
-
-
 CREATE TABLE Hrac(
-RodneCislo Number NOT NULL PRIMARY KEY,
 Prezyvka VARCHAR2(50) NOT NULL,
 Hra Number NOT NULL ,
 Vybavenie Number NOT NULL ,
 Klan Number NOT NULL ,
-Tim Number NOT NULL ,
+Nazov_Timu VARCHAR2(50) NOT NULL ,
+Osoba NUMBER NOT NULL,
+CONSTRAINT hrac_osoba_FK FOREIGN KEY (Osoba) REFERENCES Osoba,
 CONSTRAINT hrac_hra_FK FOREIGN KEY (Hra) REFERENCES Hra,
 CONSTRAINT hrac_vybavenie_FK FOREIGN KEY (Vybavenie) REFERENCES Vybavenie,
 CONSTRAINT hrac_klan_FK FOREIGN KEY (Klan) REFERENCES Klan,
-CONSTRAINT hrac_tim_FK FOREIGN KEY (Tim) REFERENCES Tim
+CONSTRAINT hrac_tim_FK FOREIGN KEY (Nazov_Timu) REFERENCES Tim
 );
 
 ---------------------------------------------
 
 CREATE TABLE Tim_sa_zucastni_turnaja(
-  ID_timu Number NOT NULL,
-  "ID_Turnaju" Number NOT NULL
+  Nazov_Timu VARCHAR2(50) NOT NULL,
+  "ID_Turnaju" Number NOT NULL,
+  CONSTRAINT tim_sa_zucastni_turnaju_FK FOREIGN KEY (Nazov_Timu) REFERENCES Tim,
+  CONSTRAINT zucastni_turnaju_FK FOREIGN KEY ("ID_Turnaju") REFERENCES Turnaj
 );
 
 CREATE TABLE Tim_sa_zucastni_zapasu(
-  ID_timu Number NOT NULL,
-  ID_Zapasu Number NOT NULL
+  Nazov_Timu VARCHAR2(50) NOT NULL,
+  ID_Zapasu Number NOT NULL,
+  CONSTRAINT tim_sa_zucastni_zapasu_FK FOREIGN KEY (Nazov_Timu) REFERENCES Tim,
+  CONSTRAINT zucastni_zapasu_FK FOREIGN KEY (ID_Zapasu) REFERENCES Zapas
 );
 
 CREATE TABLE Tim_vyhral_zapas(
-  ID_Timu Number NOT NULL ,
+  Nazov_Timu VARCHAR2(50) NOT NULL ,
   ID_Zapasu Number NOT NULL,
   skore_vyhercu Number,
-  skore_prehraneho Number
+  skore_prehraneho Number,
+  CONSTRAINT tim_vyhral_zapas_FK FOREIGN KEY (Nazov_Timu) REFERENCES Tim,
+  CONSTRAINT vyhral_zapas_FK FOREIGN KEY (ID_Zapasu) REFERENCES Zapas
+
 );
 
 CREATE TABLE Tim_vyhral_turnaj(
-  ID_Timu Number NOT NULL ,
-  "ID_Turnaju" Number NOT NULL
+  Nazov_Timu VARCHAR2(50) NOT NULL ,
+  "ID_Turnaju" Number NOT NULL,
+  CONSTRAINT tim_vyhral_turnaj_FK FOREIGN KEY (Nazov_Timu) REFERENCES Tim,
+  CONSTRAINT vyhral_turnaj_FK FOREIGN KEY ("ID_Turnaju") REFERENCES Turnaj
 );
 
 
 -------------------------------------
 
-
+INSERT INTO Osoba VALUES (9003015262,'Philip','Aram','American',TO_DATE('1990-03-01','yyyy-mm-dd'));
 INSERT INTO Osoba VALUES (9804013750,'Jakub','Vins','Slovak',TO_DATE('1998-04-01','yyyy-mm-dd'));
 INSERT INTO Hra VALUES (11111111,'DOTA2','MOBA',TO_DATE('2013-06-09','yyyy-mm-dd'),'','Valve');
 INSERT INTO Vybavenie VALUES (11111111,'A4Tech Bloody V7M Ultra Core 2','Dell KB-216','MSI Radeon RX 580 ARMOR 8G','Yenkee YHP 3010 Hornet ');
-INSERT INTO Klan VALUES (11111111,'Evil Geniuses','Medzinarodna','Everybody loves the sunshine','EG','Phillip Aram',11111111);
-INSERT INTO Tim VALUES  (11111111,'Selpice_rulz');
-INSERT INTO Tim VALUES  (22222222,'Fukusky');
+INSERT INTO Klan VALUES (11111111,'Evil Geniuses','Medzinarodna','Everybody loves the sunshine','EG',11111111,9003015262);
+INSERT INTO Tim VALUES  ('Selpice rulz');
+INSERT INTO Tim VALUES  ('Fukusky');
 INSERT INTO Turnaj VALUES (11111111,'Motorku');
 INSERT INTO Zapas VALUES (11111111,1842,'Normal',11111111);
-INSERT INTO Hrac VALUES (9804013750,'Vinso',11111111,11111111,11111111,11111111);
-INSERT INTO Tim_sa_zucastni_zapasu VALUES (11111111,11111111);
-INSERT INTO Tim_sa_zucastni_zapasu VALUES (22222222,11111111);
-INSERT INTO Tim_vyhral_zapas VALUES (11111111,11111111,10,8);
+INSERT INTO Hrac VALUES ('Vinso',11111111,11111111,11111111,'Selpice rulz',9804013750);
+INSERT INTO Tim_sa_zucastni_zapasu VALUES ('Selpice rulz',11111111);
+INSERT INTO Tim_sa_zucastni_zapasu VALUES ('Fukusky',11111111);
+INSERT INTO Tim_vyhral_zapas VALUES ('Selpice rulz',11111111,10,8);
 
